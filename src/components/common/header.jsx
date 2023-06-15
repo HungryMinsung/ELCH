ccd0412bf6c9a95da3c6461ddd9d5e6b42bb4e1d
Starving._.Minsung <kms73033222@gmail.com>
2023-06-15 22:04:50 +0900

    feat : Sign In 퍼블리싱

---
+++ src/components/common/header.jsx
@@ -0,0 +1,13 @@
+import { View, StyleSheet } from "react-native";
+function Header() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "60px",
+  },
+});
+
+export default Header;
